17aa72fa842c0a7fe045c3d7e23764f9d85ac006
agushinn <rys.agustin@gmail.com>
2024-09-28 00:09:07 -0300

    create Header component and install sass and set styles aplication

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from '@styles/components/Header/Header.module.scss'
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.appTitle}>Tic Tac</h1>
+        </header>
+    )
+}
+
+export { Header }
