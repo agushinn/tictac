b909af668aec2905617e955e4ba78a823f467975
agushinn <rys.agustin@gmail.com>
2024-10-15 12:16:01 -0300

    create log component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,39 @@ import { useState } from 'react'
 import { Header } from '@components/Header/Header'
 import { PlayersContainer } from '@components/Player/PlayersContainer'
 import { Board } from '@components/Board/Board'
+import { Log } from '@components/Log/Log'
 
 import { SYMBOL_X, SYMBOL_O, INITIAL_GAME_BOARD } from '@utils/constants'
 
 function App() {
     const [board, setBoard] = useState(INITIAL_GAME_BOARD)
     const [playerActive, setPlayerActive] = useState(SYMBOL_X)
+    const [turns, setTurns] = useState([])
 
     const handleSelectPlayer = (row, col) => {
         setPlayerActive((prevPlayer) =>
             prevPlayer === SYMBOL_X ? SYMBOL_O : SYMBOL_X
         )
+
+        setTurns((prevTurns) => {
+            // Ensure that we take the latest playerActive state
+            let currentPlayer = SYMBOL_X
+
+            if (prevTurns.length > 0 && prevTurns[0].turn === SYMBOL_X) {
+                currentPlayer = SYMBOL_O
+            }
+
+            const updateTurns = [
+                {
+                    turn: currentPlayer,
+                    row,
+                    col,
+                },
+                ...prevTurns,
+            ]
+
+            return updateTurns
+        })
 
         setBoard((prevBoard) => {
             // deep copy of the board to prevent mutation
@@ -35,6 +57,7 @@ function App() {
                     onUpdatePlayerActive={handleSelectPlayer}
                 />
             </div>
+            <Log logs={turns} />
         </>
     )
 }
