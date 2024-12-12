8080794b149f20d6d78a4bb6974c3bd697c929ec
agushinn <rys.agustin@gmail.com>
2024-12-12 20:01:48 -0300

    add winner combinations and winner or draw result function

---
+++ src/utils/winningCombinations.js
@@ -0,0 +1,42 @@
+export const WINNING_COMBINATIONS = [
+    [
+        { row: 0, column: 0 }, // x x x
+        { row: 0, column: 1 }, // - - -
+        { row: 0, column: 2 }, // - - -
+    ],
+    [
+        { row: 1, column: 0 }, // - - -
+        { row: 1, column: 1 }, // x x x
+        { row: 1, column: 2 }, // - - -
+    ],
+    [
+        { row: 2, column: 0 }, // - - -
+        { row: 2, column: 1 }, // - - -
+        { row: 2, column: 2 }, // x x x
+    ],
+    [
+        { row: 0, column: 0 }, // x - -
+        { row: 1, column: 0 }, // x - -
+        { row: 2, column: 0 }, // x - -
+    ],
+    [
+        { row: 0, column: 1 }, // - x -
+        { row: 1, column: 1 }, // - x -
+        { row: 2, column: 1 }, // - x -
+    ],
+    [
+        { row: 0, column: 2 }, // - - x
+        { row: 1, column: 2 }, // - - x
+        { row: 2, column: 2 }, // - - x
+    ],
+    [
+        { row: 0, column: 0 }, // x - -
+        { row: 1, column: 1 }, // - x -
+        { row: 2, column: 2 }, // - - x
+    ],
+    [
+        { row: 0, column: 2 }, // - - x
+        { row: 1, column: 1 }, // - x -
+        { row: 2, column: 0 }, // x - -
+    ],
+]
